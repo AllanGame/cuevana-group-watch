9710ec5424559adadf498c3c36f8b880f7273520
AllanGame <allangameyt28@gmail.com>
2021-12-27 15:07:23 -0500

    Add href property to buttons

--- src/components/button.tsx
+++ src/components/button.tsx
@@ -1,4 +1,5 @@
 import { NextPage } from "next";
+import { useRouter } from "next/router";
 import { MouseEventHandler } from "react";
 import styles from '../styles/components/Button.module.css';
 
@@ -6,11 +7,27 @@ interface Props {
     type: 'primary' | 'secondary'
     onClick?: MouseEventHandler<HTMLDivElement>
     className?: string;
+    href?: string;
 }
 
+/**
+ * Custom button
+ * You can use the href property or a custom 
+ * function to handle user click
+ * 
+ * @param props Properties of this element
+ * @returns Element
+ */
 const Button: NextPage<Props> = (props): JSX.Element => { 
+
+    const {push} = useRouter();
+
+    function hrefHandler() {
+        push(props.href || "/");
+    }
+
     return (
-        <div onClick={props.onClick} className={styles[props.type+'ButtonContainer']}>
+        <div onClick={props.onClick ? props.onClick : hrefHandler} className={styles[props.type+'ButtonContainer']}>
             <p className={styles[props.type]}>{props.children}</p>
         </div>
     )
