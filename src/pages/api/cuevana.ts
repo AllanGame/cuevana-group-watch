2dc84b36e538b1f73335f33cc8b00b2eb540d397
AllanGame <allangameyt28@gmail.com>
2022-01-02 20:29:05 -0500

    heroku debug v5

--- src/pages/api/cuevana.ts
+++ src/pages/api/cuevana.ts
@@ -43,7 +43,7 @@ export default async function handler(
 
         const tomatometelaAnonymizerResponse = await axios({
           method: "get",
-          url: nextUrl
+          url: 'https:'+nextUrl
         });
 
         const tomatometelaAnonymizer = cheerio.load(
